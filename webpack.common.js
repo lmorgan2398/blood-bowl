f8445f6f989aaad5df5be24937d5b4e672960b88
lmorgan2398 <lmorgan2398@gmail.com>
2025-08-04 20:07:17 +0000

    Add autocomplete attribute and public path to output

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+        publicPath: '/blood-bowl/',
         clean: true,
     },
     plugins: [
